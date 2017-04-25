e7f3b3ee32bc1c1cf36398b5cde87c725d981621
Christian Zufferey <christian@zufferey.com>
2017-04-25 15:31:18 +0200

    Update zdyndns_daemon_syno.sh

--- zdyndns_daemon_syno.sh
+++ zdyndns_daemon_syno.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
+#Petit daemon qui démarre au boot du Synology pourle zDynDNS à mettre dans:
 #cat /usr/local/etc/rc.d/zdyndns_daemon_syno.sh
-
-
+#zf170425.1530
 
 case $1 in
     start)
